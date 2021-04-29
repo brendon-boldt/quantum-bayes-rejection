efb0f3cda8a164a4a9d804f8245b0172ad5c9972
Brendon Boldt <brendon.mb@protonmail.com>
2021-04-29 11:28:02 -0400

    Add statevector simulation code

--- qbr/builder.py
+++ qbr/builder.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Mapping
+from typing import Mapping, Any
 
 import qiskit as qk  # type: ignore
 from qiskit.visualization import *  # type: ignore
@@ -38,9 +38,11 @@ def q_sample_prep(net: Network, inv: bool = False) -> qk.circuit.Gate:
     gate = circ.to_gate()
     if inv:
         gate = gate.inverse()
-        gate.name = "$A_p^\dagger$"
+        gate.name = "apd"
+        gate.label = "$A_p^\dagger$"
     else:
-        gate.name = "$A_p$"
+        gate.name = "ap"
+        gate.label = "$A_p$"
     return gate
 
 
@@ -61,7 +63,8 @@ def phase_flip(n_qubits: int, evidence: str) -> qk.circuit.Gate:
         if r_ev[i] == "0":
             circ.x(qreg[i])
     gate = circ.to_gate()
-    gate.name = "PF"
+    gate.name = "pf"
+    gate.label = "PF"
     return gate
 
 
@@ -72,11 +75,14 @@ def amplification(n_qubits: int) -> qk.circuit.Gate:
     circ.mcp(np.pi, qreg[:-1], qreg[-1])
     circ.x(qreg)
     gate = circ.to_gate()
-    gate.name = "AA"
+    gate.name = "aa"
+    gate.label = "AA"
     return gate
 
 
-def make_circuit(net: Network, evidence: str, n_grover_iters: int) -> qk.QuantumCircuit:
+def make_circuit(
+    net: Network, evidence: str, n_grover_iters: int, measure: bool = True
+) -> qk.QuantumCircuit:
     """Make circuit for condition rejection sampling.
 
     Arguments
@@ -99,8 +105,9 @@ def make_circuit(net: Network, evidence: str, n_grover_iters: int) -> qk.Quantum
         circ.append(amplification(len(net)), qreg)
         circ.append(q_sample_prep(net), qreg)
 
-    for i in range(len(net)):
-        circ.measure(i, i)
+    if measure:
+        for i in range(len(net)):
+            circ.measure(i, i)
 
     return circ
 
@@ -115,3 +122,13 @@ def simulate_network(
     to_pdf = lambda v: v / shots if pdf else v
     counts = {int(k, 2): to_pdf(v) for k, v in result.get_counts().items()}
     return defaultdict(lambda: 0.0, counts)
+
+
+def simulate_network_statevector(
+    net: Network, evidence: str, n_grover_iters: int, pdf: bool = False
+) -> Any:
+    circuit = make_circuit(net, evidence, n_grover_iters, measure=False)
+    backend = qk.Aer.get_backend("statevector_simulator")
+    shots = 1 << 14
+    result = qk.execute(circuit, backend, shots=shots).result()
+    return result
